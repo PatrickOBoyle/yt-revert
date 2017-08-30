0cc7a085bffa4a97ff0e477cfbc5a1349d2ab6fc
Patrick O'Boyle <oboylepa@tcd.ie>
2017-08-30 21:44:25 +0100

    :ambulance: Re-fix async calls, remove debugging

--- pageLoad.js
+++ pageLoad.js
@@ -1,8 +1,7 @@
-//
+// listen for page navigations, detect if they're YT & include our disable
 chrome.webNavigation.onBeforeNavigate.addListener((details) => {
-  console.log(details);
-
   if(detectYTURL(details.url) && details.url.indexOf("disable_polymer=true") === -1){
+    // append the disabling field
     if(details.url.indexOf("?") !== -1){
       chrome.tabs.update(details.tabId, {
         url: details.url + "&disable_polymer=true"
@@ -15,9 +14,8 @@ chrome.webNavigation.onBeforeNavigate.addListener((details) => {
   }
 });
 
+// helper to keep things clean 👍
 function detectYTURL(url){
-  return (
-          (url.indexOf("www.youtube.com") !== -1 || url.indexOf("https://youtube.com" !== -1))
-          && url.indexOf("/js") === -1
-          );
+  return ((url.indexOf("www.youtube.com") !== -1 || url.indexOf("https://youtube.com") !== -1)
+          && url.indexOf("/js") === -1);
 }
